631eec6861f895fa09b492c2a8aefb015c8ec701
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-05-30 08:42:37 -0300

    Day one finished

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,20 @@
 
-print("I love to study Python")
-print('Estudiar con "Python Total" es super divertido')
-print(500+55)
-
 #strings 
 
-print('soy un string')
+#print('soy un string')
 
 #concatenar strings
-print("hola"+' '+'mundo')
+#print("hola"+' '+'mundo')
 # alternativas para las  comillas 
-print("This is a line \n and this is other")
+#print("This is a line \n and this is other")
+#print(" Línea 1 \n Línea 2\n Línea 3")
+#print(" A   B\t C"+"\n"+" D\tE\t F"+"\n"+" G\tH\t I")
+ 
+#print(input("Tell me your name "))
+#input(print("¿Qué estás estudiando?"))
+#print(input("¿Qué estás estudiando?"))
+
+#print(input("Escribe tu nombre:")+" "+input("Escribe tu apellido: "))
+#print(input("Escribe tu nombre:") + " " + input("Escribe tu apellido:"))
+print("Responda estas dos preguntas para nombrar su cerveza\n")
+print("El nombre de su mascota es "+""+input("¿Nombre de su perro?: ")+input("Nombre de su animal favorito: "))
