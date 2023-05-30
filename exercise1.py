4bd8a55a0b01407c344a7e997d9b5a3fcf060690
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-05-30 00:36:43 -0300

    strings practice

--- exercise1.py
+++ exercise1.py
@@ -1,2 +1,13 @@
 
 print("I love to study Python")
+print('Estudiar con "Python Total" es super divertido')
+print(500+55)
+
+#strings 
+
+print('soy un string')
+
+#concatenar strings
+print("hola"+' '+'mundo')
+# alternativas para las  comillas 
+print("This is a line \n and this is other")
