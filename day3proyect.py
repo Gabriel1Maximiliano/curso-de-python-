96cd4a7eabf3fc491c9c00e849a8fd63d979e243
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-05-31 12:26:00 -0300

    variable names changed

--- day3proyect.py
+++ day3proyect.py
@@ -1,33 +1,25 @@
-# first step the user input a phrase 
 phrase = "Le vAs a decir al usuario cuántas palabras hay a lo largo de todo el texto"
-#second the user input 3 letters
-lettersToSeach = ['a','b','c']
+lettersToSearch = ['a', 'b', 'c']
 
-amount_of_firstLetter  = phrase.lower().count( lettersToSeach[0].lower() )
-amount_of_secondLetter = phrase.lower().count( lettersToSeach[1].lower() )
-amount_of_thirdLetter  = phrase.lower().count( lettersToSeach[2].lower() )
-#thrid we count the amount of words in this phrase
+count_of_firstLetter = phrase.lower().count(lettersToSearch[0].lower())
+count_of_secondLetter = phrase.lower().count(lettersToSearch[1].lower())
+count_of_thirdLetter = phrase.lower().count(lettersToSearch[2].lower())
 
-amount_of_words = len( phrase.split(" "))
-print( "Welcome to Day 3 Python course" )
+word_count = len(phrase.split(" "))
+print("Welcome to Day 3 Python course")
 
-print( f"The amount of '{lettersToSeach[0]}' is {amount_of_firstLetter}" )
-print( f"The amount of '{lettersToSeach[1]}' is {amount_of_secondLetter}" )
-print( f"The amount of '{lettersToSeach[2]}' is {amount_of_thirdLetter}" )
-print( f"The amount of words is { amount_of_words }\n")
+print(f"The count of '{lettersToSearch[0]}' is {count_of_firstLetter}")
+print(f"The count of '{lettersToSearch[1]}' is {count_of_secondLetter}")
+print(f"The count of '{lettersToSearch[2]}' is {count_of_thirdLetter}")
+print(f"The count of words is {word_count}\n")
 
-# fourth we need the first and the last letter of the phrase
-first_phrase_letter       =  phrase[0] 
-last_letter_of_the_phrase = phrase[len(phrase) - 1] 
-print( f"The first letter of the phrase is: { first_phrase_letter  }\n" )
-print( f"The last letter of the phrase is: { last_letter_of_the_phrase }\n" )
+first_letter = phrase[0]
+last_letter = phrase[len(phrase) - 1]
+print(f"The first letter of the phrase is: {first_letter}\n")
+print(f"The last letter of the phrase is: {last_letter}\n")
 
-#reverese phrase
-print("Reverse phrase")
 reverse_phrase = phrase[::-1]
-print( f"'{reverse_phrase}'" )
+print("Reverse phrase")
+print(f"'{reverse_phrase}'")
 print("Is the word Python in this text?")
-print( "Python" in phrase )
-
-
-
+print("Python" in phrase)
